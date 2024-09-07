b090b33221ba0c7cdee8997978e7b78539e70b34
Pablo <2005pcgv@gmai.com>
2024-09-06 21:27:10 -0600

    starting

---
+++ main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.*;
+public class main {
+  public static void main(String[] args)throws IOException {
+    Scanner teclado = new Scanner(System.in);
+    int dividendo;
+    int divisor;
+    int residuo;
+    int cociente;
+    System.out.println("Bienvenido al programa de división");
+    System.out.println("Ingrese el dividendo");
+    dividendo = teclado.nextInt();
+    System.out.println("Ingrese el divisor");
+    divisor = teclado.nextInt();
+    cociente = dividendo/divisor;
+    residuo = dividendo % divisor;
+
